5badb98374f5bfe4629ac66276219993dd4bef6c
Graddlly <34636667+Graddlly@users.noreply.github.com>
2022-06-02 12:35:58 +0500

    [FIX] Исправление проверки получения информации из .env

--- DBUtils/DBUtils.cs
+++ DBUtils/DBUtils.cs
@@ -8,33 +8,34 @@ using MySql.Data.MySqlClient;
 
 namespace AcademicApplicants.DBUtils;
 
+// ReSharper disable once InconsistentNaming
+// ReSharper disable once ClassNeverInstantiated.Global
 public class DBUtils
 {
     public static MySqlConnection GetDbConnection()
     {
-        using (var stream = File.OpenRead($"{Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName}/DBUtils/.env"))
+        try
         {
+            using var stream =
+                File.OpenRead(
+                    $"{Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName}/DBUtils/.env");
             DotNetEnv.Env.Load(stream);
         }
-        
+        catch (Exception error) { ShowErrorMessage(error.Message); }
+
         var host = DotNetEnv.Env.GetString("HOST") ?? string.Empty;
         var port = DotNetEnv.Env.GetInt("PORT");
         var database = DotNetEnv.Env.GetString("DATABASE") ?? string.Empty;
         var username = DotNetEnv.Env.GetString("USERNAME") ?? string.Empty;
         var password = DotNetEnv.Env.GetString("PASSWORD") ?? string.Empty;
-
-        // ReSharper disable once InvertIf
-        if (host == string.Empty || database == string.Empty ||
-            username == string.Empty || password == string.Empty)
-        {
-            ShowErrorMessage();
-            Environment.Exit(0);
-        }
         
+        if (host == string.Empty || database == string.Empty ||
+            username == string.Empty || password == string.Empty) Environment.Exit(0);
+
         return DBMySQLUtils.GetDbConnection(host, port, database, username, password);
     }
     
-    private static void ShowErrorMessage()
+    private static void ShowErrorMessage(string error)
     {
         var msgBoxError = MessageBox.Avalonia.MessageBoxManager
             .GetMessageBoxCustomWindow(new MessageBoxCustomParamsWithImage
@@ -43,7 +44,7 @@ public class DBUtils
                 CanResize = false,
                 Height = 250,
                 WindowIcon = new WindowIcon($"{Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName}/Assets/chelNew.png"),
-                ContentMessage = "Системная ошибка: Проверьте подключение к .env и повторите попытку!",
+                ContentMessage = "Системная ошибка: Проверьте подключение к .env и повторите попытку!\n" + error,
                 Icon = new Bitmap($"{Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName}/Assets/cancel.png"),
                 ButtonDefinitions = new[] 
                 {
