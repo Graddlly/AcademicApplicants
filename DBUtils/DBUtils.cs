7080c722176d3fb3baf4d3ee63242c7f40f10e8c
Graddlly <34636667+Graddlly@users.noreply.github.com>
2022-06-02 12:18:08 +0500

    [FIX] Добавление информации для подключения БД в .env

--- DBUtils/DBUtils.cs
+++ DBUtils/DBUtils.cs
@@ -1,3 +1,9 @@
+using System;
+using System.IO;
+using Avalonia.Controls;
+using Avalonia.Media.Imaging;
+using MessageBox.Avalonia.DTO;
+using MessageBox.Avalonia.Models;
 using MySql.Data.MySqlClient;
 
 namespace AcademicApplicants.DBUtils;
@@ -6,12 +12,48 @@ public class DBUtils
 {
     public static MySqlConnection GetDbConnection()
     {
-        const string host = "localhost";
-        const int port = 3306;
-        const string database = "AcademicApplicants";
-        const string username = "AcademicApp";
-        const string password = "7896";
+        using (var stream = File.OpenRead($"{Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName}/DBUtils/.env"))
+        {
+            DotNetEnv.Env.Load(stream);
+        }
+        
+        var host = DotNetEnv.Env.GetString("HOST") ?? string.Empty;
+        var port = DotNetEnv.Env.GetInt("PORT");
+        var database = DotNetEnv.Env.GetString("DATABASE") ?? string.Empty;
+        var username = DotNetEnv.Env.GetString("USERNAME") ?? string.Empty;
+        var password = DotNetEnv.Env.GetString("PASSWORD") ?? string.Empty;
 
+        // ReSharper disable once InvertIf
+        if (host == string.Empty || database == string.Empty ||
+            username == string.Empty || password == string.Empty)
+        {
+            ShowErrorMessage();
+            Environment.Exit(0);
+        }
+        
         return DBMySQLUtils.GetDbConnection(host, port, database, username, password);
+    }
+    
+    private static void ShowErrorMessage()
+    {
+        var msgBoxError = MessageBox.Avalonia.MessageBoxManager
+            .GetMessageBoxCustomWindow(new MessageBoxCustomParamsWithImage
+            {
+                ShowInCenter = true,
+                CanResize = false,
+                Height = 250,
+                WindowIcon = new WindowIcon($"{Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName}/Assets/chelNew.png"),
+                ContentMessage = "Системная ошибка: Проверьте подключение к .env и повторите попытку!",
+                Icon = new Bitmap($"{Directory.GetParent(Environment.CurrentDirectory)?.Parent?.Parent?.FullName}/Assets/cancel.png"),
+                ButtonDefinitions = new[] 
+                {
+                    new ButtonDefinition
+                    {
+                        Name = "Ok", IsDefault = true, IsCancel = false
+                    }
+                },
+                WindowStartupLocation = WindowStartupLocation.CenterOwner
+            });
+        msgBoxError.Show();
     }
 }
